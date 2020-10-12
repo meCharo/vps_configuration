25a79b79fc91ed9a31788af4cd093e6f67ba6c49
meCharo <mecharo21@icloud.com>
2020-10-13 07:46:04 +0800

    Update vps_installation.sh

--- vps_installation.sh
+++ vps_installation.sh
@@ -27,6 +27,7 @@ if [ $your_name == "shadowsocks" ];then
 elif [ $your_name == "v2ray" ];then
 #   curl -Ls https://install.direct/go.sh | sudo bash
   curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+  bash install-release.sh
   rm -f install-release.sh
 #   wget https://raw.githubusercontent.com/meCharo/vps_configuration/master/v2ray/config.json -O /etc/v2ray/config.json
   wget https://raw.githubusercontent.com/meCharo/vps_configuration/master/v2ray/config.json -O /usr/local/etc/v2ray/config.json
