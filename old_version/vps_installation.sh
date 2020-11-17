8949e1af13cb8dcbc2681ee80fa4461739159488
Changhao Liu <charo@Charo-Mac.local>
2020-11-17 14:29:49 +0800

    schedule frame

--- old_version/vps_installation.sh
+++ old_version/vps_installation.sh
@@ -1,37 +1,16 @@
-#!/bin/sh
-
 #################################
 #########preparation#############
 #################################
 mkdir /root/temp && cd /root/temp
 yum update -y
-#-----------system---------------
-yum install -y zip unzip wget
-echo "export LC_ALL=en_US.UTF-8" >> $HOME/.bashrc
-echo "export LANG=en_US.UTF-8" >> $HOME/.bashrc
-
+yum install wget gcc gcc-c++ make unzip python36 -y
 #----------firework--------------
 # firewall-cmd --permanent --add-port=25000/tcp # only centos7
 # firewall-cmd --reload # reload after modify
 # iptables -A INPUT -p tcp --dport 25000 -j ACCEPT # only centos6
 # chkconfig iptables off
-systemctl stop firewalld
-systemctl disable firewalld
-
-
-#################################
-########### python ##############
-#################################
-cd /root/temp
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3.sh
-bash Miniconda3.sh -b -p $HOME/Applications/miniconda
-source $HOME/Applications/miniconda/bin/activate
-conda init
-source ~/.bashrc
-conda create -n py python=3.6 -y
-conda activate py
-cd /root/temp && rm -rf Miniconda3.sh
-
+# systemctl stop firewalld
+# systemctl disable firewalld
 
 #################################
 #############proxy###############
@@ -59,24 +38,50 @@ else
   exit 1
 fi
 
-
 #################################
 ########download tools###########
 #################################
 #---------youtube-dl-------------
-conda install ffmpeg -y
-pip install youtube-dl
-
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+#------------yasm----------------
+cd /root/temp
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar -zxvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
+./configure
+make
+make install
+cd /root/temp && rm -rf yasm-1.3.0 && rm -rf yasm-1.3.0.tar.gz
+#------------ffmpeg--------------
+cd /root/temp
+wget http://www.ffmpeg.org/releases/ffmpeg-3.3.3.tar.gz
+tar -zxvf ffmpeg-3.3.3.tar.gz
+cd ffmpeg-3.3.3
+./configure
+make
+make install
+cd /root/temp && rm -rf ffmpeg-3.3.3 && rm -rf ffmpeg-3.3.3.tar.gz
+#------------gdrive--------------
+# wget -O gdrive https://sites.google.com/site/wun913/Home/gdrive-linux-x64
+# mv gdrive /usr/bin/gdrive
+# chmod +x /usr/bin/gdrive
+# Usage: https://github.com/gdrive-org/gdrive
+# Usage: gdrive about->gdrive list->gdrive download ..
 
 #################################
-########transfer tools###########
+###########upload tools##########
 #################################
-yum -y install vsftpd
-systemctl start vsftpd.service
+#------------BaiduPCS------------
+# cd /root/temp
+# wget https://github.com/iikira/BaiduPCS-Go/releases/download/v3.6/BaiduPCS-Go-v3.6-linux-amd64.zip
+# unzip BaiduPCS-Go-v3.6-linux-amd64.zip
+# mv BaiduPCS-Go-v3.6-linux-amd64 /usr/local/bin/BaiduPCS-Go_bin
+# chmod a+rx /usr/local/bin/BaiduPCS-Go_bin
+# ln -s /usr/local/bin/BaiduPCS-Go_bin/BaiduPCS-Go /usr/local/bin/BaiduPCS-Go
+# cd /root/temp && rm -f BaiduPCS-Go-v3.6-linux-amd64.zip
+# Usage: BaiduPCS-Go login->BaiduPCS-Go u->BaiduPCS-Go download
 
-
-#################################
-#######post-installation#########
-#################################
-cd /root && rm -rf temp && rm -f create_vps.sh
+# post-installation
+cd /root && rm -rf temp && rm -f vps_installation.sh
 
